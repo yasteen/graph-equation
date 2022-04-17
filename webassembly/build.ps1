e28b0311969d10da997397a1a532636134f61ea1
yasteen <justinchan135@gmail.com>
2022-04-17 13:43:15 -0700

    fix building wasm

--- webassembly/build.ps1
+++ webassembly/build.ps1
@@ -1,3 +1,5 @@
+
+go mod tidy
 $Env:GOOS = "js"; $Env:GOARCH = "wasm"; go build -o ..\public\scripts\main.wasm
 Remove-Item Env:\GOOS
 Remove-Item Env:\GOARCH
--- webassembly/build.sh
+++ webassembly/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+go mod tidy
 GOOS=js GOARCH=wasm go build -o ../public/scripts/main.wasm
 echo "Finished building Go WASM module."
